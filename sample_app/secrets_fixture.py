e75ece97058b197229a1750f9cccc95f28455dbd
Sureshsingh7 <sureshsinghpanwar777@gmail.com>
2026-05-19 21:02:55 +0530

    refined trigger conditions

--- sample_app/secrets_fixture.py
+++ sample_app/secrets_fixture.py
@@ -4,7 +4,7 @@ These values match common provider token formats so scanners can detect them.
 They are not valid credentials.
 """
 
-GITHUB_PAT = "ghp_AbCdEfGhIjKlMnOpQrStUv_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw"
+GITHUB_PAT = "ghp_AbCdEfGhIjKlMnOpQrStUv_0123456789"
 az_SECRET_ACCESS_KEY = "7f9c2a1b3d4e5f60718293ab4cd5ef67"
 az_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
 SLACK_BOT_TOKEN = "SLACK_BOT_TOKEN_PLACEHOLDER"
